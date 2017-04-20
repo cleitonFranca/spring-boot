a8c507dc545f441ba960e550ed5c1d9f2f330709
root <root@debian.local.br>
2017-04-20 16:48:55 -0300

    TESTES

--- torcedor.digital/src/test/java/servidor/torcedor/digital/AppTest.java
+++ torcedor.digital/src/test/java/servidor/torcedor/digital/AppTest.java
@@ -1,8 +1,21 @@
 package servidor.torcedor.digital;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 /**
  * Unit test for simple App.
  */
+
 public class AppTest {
-    
+
+	@Test
+	public void test() {
+		int a = 4;
+		int b = 3;
+		assertTrue(a > b);
+
+	}
+
 }
