bfc7aca78263c4c046af098f79eec3fec4191996
Cleiton França <cleiton2281@gmail.com>
2017-07-18 22:17:01 -0300

    Criação do método para envio de email com template html

--- torcedor.digital/src/main/java/servidor/torcedor/digital/utils/SenderMailService.java
+++ torcedor.digital/src/main/java/servidor/torcedor/digital/utils/SenderMailService.java
@@ -1,8 +1,12 @@
 package servidor.torcedor.digital.utils;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -32,6 +36,28 @@ public class SenderMailService {
         email.setText(conteudo);
      
         mailSender.send(email);
+    	
+    }
+    
+    /**
+     * Método para envio de e-mail com template em html
+     * @param emailPara: email do destinatário
+     * @param titulo: título do e-mail
+     * @param conteudo: conteúdo html do e-mail
+     * @throws MessagingException
+     */
+    public void sendHtmlTemplate(String emailPara, String titulo, String conteudo) throws MessagingException {
+  
+    	MimeMessage mimeMessage = mailSender.createMimeMessage();
+    	MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+    	String htmlMsg = conteudo;
+    	mimeMessage.setContent(htmlMsg, "text/html");
+    	
+    	helper.setTo(emailPara);
+    	helper.setSubject(titulo);
+    	helper.setFrom("cleiton2281@gmail.com");
+
+        mailSender.send(mimeMessage);
     	
     }
     
---
+++ torcedor.digital/src/test/java/servidor/torcedor/digital/TestVelocityTemplateForEmail.java
@@ -0,0 +1,84 @@
+package servidor.torcedor.digital;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import servidor.torcedor.digital.utils.SenderMailService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestVelocityTemplateForEmail {
+	
+	 @Autowired
+	 SenderMailService senderMailService;
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test
+	public void test_email_com_template() throws Exception {
+	        /*
+	         *   first, get and initialize an engine
+	         */
+	        VelocityEngine ve = new VelocityEngine();
+	        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+	        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+	        ve.init();
+
+	        /*
+	         *   organize our data 
+	         */
+	        ArrayList list = new ArrayList();
+	        Map map = new HashMap();
+
+	        map.put("name", "Cow");
+	        map.put("price", "$100.00");
+	        list.add( map );
+	 
+	        map = new HashMap();
+	        map.put("name", "Eagle");
+	        map.put("price", "$59.99");
+	        list.add( map );
+
+	        map = new HashMap();
+	        map.put("name", "Shark");
+	        map.put("price", "$3.99");
+	        list.add( map );
+
+	        /*
+	         *  add that list to a VelocityContext
+	         */
+	        VelocityContext context = new VelocityContext();
+	        context.put("petList", list);
+
+	        /*
+	         *   get the Template  
+	         */      
+	        Template t = ve.getTemplate( "/static/templates/email_tmpl.vm" );
+
+	        /*
+	         *  now render the template into a Writer, here 
+	         *  a StringWriter 
+	         */
+	        StringWriter writer = new StringWriter();
+	        t.merge( context, writer );
+
+	        /*
+	         *  use the output in the body of your emails
+	         */   
+	        senderMailService.sendHtmlTemplate("cleiton2281@gmail.com", "Teste Com template", writer.toString());
+	        
+	    }
+
+}
